48bffb27725282209da12cbeefee207c0d40f0b8
Megan White <mfw247@gmail.com>
2019-08-23 10:28:42 -0700

    attempted put and delete requests

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,37 +1,59 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
-  code: { hex: "" }
+  code: { hex: "" },
 };
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
   const editColor = color => {
     setEditing(true);
     setColorToEdit(color);
+    console.log(color);
   };
 
   const saveEdit = e => {
     e.preventDefault();
+    axiosWithAuth()
+    .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+    .then(res => {
+      console.log(res);
+      setColorToEdit(initialColor);
+      colors && (colors.map(color => {
+        if(color.id === colorToEdit.id) {
+          updateColors(colorToEdit)
+        }
+      }))
+      this.props.history.push('/');
+    })
+    .catch(err => console.log(err.response));
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
   };
 
-  const deleteColor = color => {
+  const deleteColor = e => {
     // make a delete request to delete this color
+    e.preventDefault();
+    axios
+      .delete(`http://localhost:5000/api/colors/${colorToEdit.id}`)
+      .then(res => {
+        updateColors(res.data);
+        this.props.history.push('/');
+      })
+      .catch(err => console.log(err.response));
   };
 
   return (
     <div className="colors-wrap">
       <p>colors</p>
       <ul>
-        {colors.map(color => (
+        {colors && (colors.map(color => (
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
               <span className="delete" onClick={() => deleteColor(color)}>
@@ -44,7 +66,7 @@ const ColorList = ({ colors, updateColors }) => {
               style={{ backgroundColor: color.code.hex }}
             />
           </li>
-        ))}
+        )))}
       </ul>
       {editing && (
         <form onSubmit={saveEdit}>
